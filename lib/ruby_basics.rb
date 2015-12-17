2de1634389d3ff02358bc19c92d29794c8b34f92
adeagboola <dannie496@gmail.com>
2015-12-17 00:42:51 -0500

    Ruby Review

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,37 @@
-def division(num1, num2)
- 
+def division(num1 = "42", num2 = "7")
+  puts "42 / 7"
+  6
+end
+division
+
+def assign_variable(name = "Bob")
+  "#{name}"
+end
+assign_variable
+
+def argue(phrase)
+  phrase
 end
 
-def assign_variable(value)
-  
-end
-
-def argue
-  
-end
-
-def greeting
-
+def greeting(greeting, name)
+  puts "#{greeting} #{name}"
 end
 
 def return_a_value
+ "Nice"
+end
+return_a_value
 
+def last_evaluated_value(phrase = "expert")
+  phrase
 end
 
-def last_evaluated_value
-
+def pizza_party(pizza = "cheese")
+  puts "#{pizza}"
 end
+pizza_party("cheese")
 
-def pizza_party
-
+def pizza_party(pizza = "cheese")
+ "#{pizza}"
 end
+pizza_party("cheese")
